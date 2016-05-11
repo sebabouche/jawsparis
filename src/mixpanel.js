4974049d9b549ba526b012869181e142fe589cc4
sebabouche <s.nicolaidis@me.com>
2016-05-11 19:22:28 +0200

    Mixpanel token for staging, production and dev

--- src/mixpanel.js
+++ src/mixpanel.js
@@ -4,8 +4,10 @@ let token = "";
 
 if (process.env.NODE_ENV == 'production') {
   token = "6ecc9cfd86674de43751d9b80b747dda";
-} else {
+} else if (process.env.NODE_ENV == 'staging') {
   token = "28bd70982351b1692070b35096235acb";
+} else {
+  token = "7bc9e9c4643bcf2ae9d74dbfd43632bc";
 }
 
 mixpanel.init(token);
