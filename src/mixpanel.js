da64c6d7ecab9c6ff55df2a0d27f69d435b72ee4
sebabouche <s.nicolaidis@me.com>
2016-05-11 18:09:28 +0200

    Removed mixpanel production env var

--- src/mixpanel.js
+++ src/mixpanel.js
@@ -1,6 +1,6 @@
 const mixpanel = require('mixpanel-browser');
 
-const token = process.env.mixpanel_token || '28bd70982351b1692070b35096235acb';
+const token = '28bd70982351b1692070b35096235acb';
 
 mixpanel.init(token);
 
