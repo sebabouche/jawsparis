303c5e1c93b33c8defff783be3bea06267021b6d
Sacha Hamiche <sacha.hamiche@gmail.com>
2016-05-17 16:16:47 +0200

    Added scroll to the Checkout

--- src/components/Checkout/index.jsx
+++ src/components/Checkout/index.jsx
@@ -1,11 +1,63 @@
 import React from 'react'
+var Scroll = require('react-scroll');
+var scroll = Scroll.animateScroll;
 
 import ShoppingBagFields from '../ShoppingBagFields'
 import DeliveryFields from '../DeliveryFields'
 import PaymentFields from '../PaymentFields'
 
 export default class Checkout extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      delivrery:false,
+      paiment:false
+    }
+    this.handleShopBagClick = this.handleShopBagClick.bind(this);
+    this.handleDelivreryClick = this.handleDelivreryClick.bind(this);
+  }
+
+  handleShopBagClick(event) {
+    this.setState({delivrery:true});
+    scroll.scrollToBottom();
+  }
+  handleDelivreryClick(event) {
+    this.setState({paiment:true});
+    scroll.scrollToBottom();
+  }
+
   render () {
+
+    var deliveryscroll = this.state.delivrery ? <section className="uk-block uk-block-large uk-animation-fade uk-animation-3">
+      <h2 className="uk-text-center">
+        <span className="uk-badge uk-badge-notification">2</span>
+        Livraison
+      </h2>
+      <DeliveryFields />
+    </section>:<button
+      className="uk-button uk-button-success uk-width-1-1 uk-container-center" onClick={this.handleShopBagClick}
+      type="button">
+      Valider votre cabas
+    </button>;
+
+    if (this.state.paiment){
+      var paimentscroll = <section className="uk-block uk-block-large uk-animation-fade uk-animation-3">
+      <h2 className="uk-text-center">
+        <span className="uk-badge uk-badge-notification">3</span>
+        Paiement
+      </h2>
+        <PaymentFields />
+      </section>;
+    } else if(!this.state.paiment && this.state.delivrery){
+      var paimentscroll = <button
+        className="uk-button uk-button-success uk-width-1-1 uk-container-center" onClick={this.handleDelivreryClick}
+        type="button">
+        Valider l'adresse de livraison
+      </button>;
+    }
+
+
     return (
       <div className="uk-container uk-container-center">
         <section className="uk-block uk-block-large">
@@ -16,20 +68,8 @@ export default class Checkout extends React.Component {
           </h2>
           <ShoppingBagFields />
         </section>
-        <section className="uk-block uk-block-large">
-          <h2 className="uk-text-center">
-            <span className="uk-badge uk-badge-notification">2</span>
-            Livraison
-          </h2>
-          <DeliveryFields />
-        </section>
-        <section className="uk-block uk-block-large">
-        <h2 className="uk-text-center">
-          <span className="uk-badge uk-badge-notification">3</span>
-          Paiement
-        </h2>
-          <PaymentFields />
-        </section>
+        {deliveryscroll}
+        {paimentscroll}
       </div>
     )
   }
