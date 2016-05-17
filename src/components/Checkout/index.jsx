975f912cf7588df8641a79b228be57542f5360ac
sebabouche <s.nicolaidis@me.com>
2016-05-17 10:38:45 +0200

    Add delivery and payment fields

--- src/components/Checkout/index.jsx
+++ src/components/Checkout/index.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
 
-import ShoppingBag from '../ShoppingBag'
-import Delivery from '../Delivery'
-import Payment from '../Payment'
+import ShoppingBagFields from '../ShoppingBagFields'
+import DeliveryFields from '../DeliveryFields'
+import PaymentFields from '../PaymentFields'
 
 export default class Checkout extends React.Component {
   render () {
     return (
-      <div>
-        <ShoppingBag />
-        <Delivery />
-        <Payment />
+      <div className="uk-container uk-container-center">
+        <section className="uk-block uk-block-large">
+          <div className="uk-text-center"></div>
+          <h2 className="uk-text-center">
+            <span className="uk-badge uk-badge-notification">1</span>
+            Votre cabas
+          </h2>
+          <ShoppingBagFields />
+        </section>
+        <section className="uk-block uk-block-large">
+          <h2 className="uk-text-center">
+            <span className="uk-badge uk-badge-notification">2</span>
+            Livraison
+          </h2>
+          <DeliveryFields />
+        </section>
+        <section className="uk-block uk-block-large">
+        <h2 className="uk-text-center">
+          <span className="uk-badge uk-badge-notification">3</span>
+          Paiement
+        </h2>
+          <PaymentFields />
+        </section>
       </div>
     )
   }
