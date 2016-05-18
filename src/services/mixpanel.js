5f8cfd63b8b8dcbb2396d10039a1616dc1f99ddf
sebabouche <s.nicolaidis@me.com>
2016-05-18 09:34:22 +0200

    Fix mixpanel env var condition

--- src/services/mixpanel.js
+++ src/services/mixpanel.js
@@ -2,9 +2,9 @@ const mixpanel = require('mixpanel-browser');
 
 let token = "";
 
-if (__PROD__) {
+if (typeof __PROD__ !== 'undefined' && __PROD__) {
   token = "6ecc9cfd86674de43751d9b80b747dda";
-} else if (__STAGING__) {
+} else if (typeof __STAGING__ !== 'undefined' && __STAGING__) {
   token = "28bd70982351b1692070b35096235acb";
 } else {
   token = "7bc9e9c4643bcf2ae9d74dbfd43632bc";
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,18 +1,18 @@
-import jsdom from 'jsdom';
-import chai from 'chai';
-import chaiImmutable from 'chai-immutable';
+import jsdom from 'jsdom'
+import chai from 'chai'
+import chaiImmutable from 'chai-immutable'
 
-import register from 'ignore-styles';
-register(['.css', '.sass', '.scss', '.png']);
+import register from 'ignore-styles'
+register(['.css', '.sass', '.scss', '.png'])
 
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
-const win = doc.defaultView;
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
+const win = doc.defaultView
 
-global.document = doc;
-global.window = win;
+global.document = doc
+global.window = win
 
 Object.keys(window).forEach((key) => {
   if (!(key in global)) {
     global[key] = window[key];
   }
-});
+})
