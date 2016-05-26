e126fecb5d712ded8cf836caa6ffebdf9baa77ba
sebabouche <s.nicolaidis@me.com>
2016-05-27 00:12:30 +0200

    Product Item icon correction + Build for staging

--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -44,7 +44,7 @@ export default class ProductItem extends Component {
 
          <div className="uk-panel-teaser">
            <a data-uk-toggle={productTargetTag}>
-               <div className="uk-panel-badge uk-icon-info-circle uk-text-contrast"></div>
+               <div className="uk-panel-badge uk-icon-commenting uk-text-contrast"></div>
              <figure className="uk-overlay">
                <img src={product.img} alt={product.title}/>
                <figcaption id={productTag} className="uk-overlay-panel uk-overlay-background uk-hidden uk-scrollable-text" aria-hidden="true" >
