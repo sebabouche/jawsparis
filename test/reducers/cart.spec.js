3da399ac61f2517a1ade99d7033616fae0747d7e
sebabouche <s.nicolaidis@me.com>
2016-06-01 04:49:10 +0200

    8 errors on cart reducer and selectors

--- test/reducers/cart.spec.js
+++ test/reducers/cart.spec.js
@@ -23,72 +23,74 @@ describe('reducers', () => {
 
     context('when ADD_TO_CART action', () => {
       it('when product is not in cart, it should add product', () => {
-        const state = {
-          addedIds: [ 1 ],
-          quantityById: { 1: 1 }
-        }
+        const state = Map({
+          addedIds: List.of(1),
+          quantityById: Map({ 1: 1 })
+        })
         const nextState = cart(state, { type: 'ADD_TO_CART', productId: 2})
 
-        expect(nextState).toEqual({
-          addedIds: [ 1, 2 ],
-          quantityById: { 1: 1, 2: 1 }
-        })
+        expect(nextState).toEqual(
+          Map({
+            addedIds: List.of(1, 2),
+            quantityById: Map({ 1: 1, 2: 1 })
+          })
+        )
       })
 
       it('when product is already in cart, it should increment quantitites', () => {
-        const state = {
-          addedIds: [ 1, 2 ],
-          quantityById: { 1: 1, 2: 1 }
-        }
+        const state = Map({
+          addedIds: List.of(1, 2),
+          quantityById: Map({ 1: 1, 2: 1 })
+        })
         const nextState = cart(state, { type: 'ADD_TO_CART', productId: 2})
 
-        expect(nextState).toEqual({
-          addedIds: [ 1, 2 ],
-          quantityById: { 1: 1, 2: 2 }
-        })
+        expect(nextState).toEqual(Map({
+          addedIds: List.of(1, 2),
+          quantityById: Map({ 1: 1, 2: 2 })
+        }))
       })
     })
 
     context('when REMOVE_FROM_CART action', () => {
       it('when not in cart, should do nothing', () => {
-        const state = {
-          addedIds: [ 1, 2 ],
-          quantityById: { 1: 1, 2: 1 }
-        }
+        const state = Map({
+          addedIds: List.of(1, 2),
+          quantityById: Map({ 1: 1, 2: 1 })
+        })
 
         const nextState = cart(state, { type: 'REMOVE_FROM_CART', productId: 3})
 
-        expect(nextState).toEqual({
-          addedIds: [ 1, 2 ],
-          quantityById: { 1: 1, 2: 1 }
-        })
+        expect(nextState).toEqual(Map({
+          addedIds: List.of(1, 2),
+          quantityById: Map({ 1: 1, 2: 1 })
+        }))
       })
 
       it('when product was once in cart, should remove product ', () => {
-        const state = {
-          addedIds: [1, 2, 3],
-          quantityById: {1: 1, 2: 1, 3: 1}
-        }
+        const state = Map({
+          addedIds: List.of(1, 2, 3),
+          quantityById: Map({1: 1, 2: 1, 3: 1})
+        })
         const nextState = cart(state, { type: 'REMOVE_FROM_CART', productId: 2})
 
-        expect(nextState).toEqual({
-          addedIds: [1, 3],
-          quantityById: {1: 1, 3: 1}
-        })
+        expect(nextState).toEqual(Map({
+          addedIds: List.of(1, 3),
+          quantityById: Map({1: 1, 3: 1})
+        }))
       })
 
       it('when product is more than once in cart, should substract product quantity ', () => {
-        const state = {
-          addedIds: [1, 2, 3],
-          quantityById: {1: 1, 2: 2, 3: 1}
-        }
+        const state = Map({
+          addedIds: List.of(1, 2, 3),
+          quantityById: Map({1: 1, 2: 2, 3: 1})
+        })
         const nextState = cart(state, { type: 'REMOVE_FROM_CART', productId: 2})
         console.log("nextState", nextState)
 
-        expect(nextState).toEqual({
-          addedIds: [1, 2, 3],
-          quantityById: {1: 1, 2: 1, 3: 1}
-        })
+        expect(nextState).toEqual(Map({
+          addedIds: List.of(1, 2, 3),
+          quantityById: Map({1: 1, 2: 1, 3: 1})
+        }))
       })
     })
   })
--- test/reducers/selectors.spec.js
+++ test/reducers/selectors.spec.js
@@ -1,67 +1,69 @@
+import {Map, List} from 'immutable'
 import expect from 'expect'
 import { getTotal, getCartProducts, getCartProductsQuantity } from '../../src/reducers'
 
 describe('selectors', () => {
   describe('getTotal', () => {
     it('should return price total', () => {
-      const state = {
-        cart: {
-          addedIds: [ 1, 2, 3 ],
-          quantityById: {
+      const state = Map({
+        cart: Map({
+          addedIds: List.of(1, 2, 3),
+          quantityById: Map({
             1: 4,
             2: 2,
             3: 1
-          }
-        },
-        products: {
-          byId: {
-            1: {
+          })
+        }),
+        products: Map({
+          byId: Map({
+            1: Map({
               id: 1,
               price: 1.99
-            },
-            2: {
+            }),
+            2: Map({
               id: 1,
               price: 4.99
-            },
-            3: {
+            }),
+            3: Map({
               id: 1,
               price: 9.99
-            }
-          }
-        }
-      }
+            })
+          })
+        })
+      })
+      
       expect(getTotal(state)).toBe('27.93')
     })
   })
 
   describe('getCartProducts', () => {
     it('should return products with quantity', () => {
-      const state = {
-        cart: {
-          addedIds: [ 1, 2, 3 ],
-          quantityById: {
+      const state = Map({
+        cart: Map({
+          addedIds: List.of( 1, 2, 3 ),
+          quantityById: Map({
             1: 4,
             2: 2,
             3: 1
-          }
-        },
-        products: {
-          byId: {
-            1: {
+          })
+        }),
+        products: Map({
+          byId: Map({
+            1: Map({
               id: 1,
               price: 1.99
-            },
-            2: {
+            }),
+            2: Map({
               id: 1,
               price: 4.99
-            },
-            3: {
+            }),
+            3: Map({
               id: 1,
               price: 9.99
-            }
-          }
-        }
-      }
+            })
+          })
+        })
+      })
 
       expect(getCartProducts(state)).toEqual([
         {
@@ -85,32 +87,32 @@ describe('selectors', () => {
 
   describe('getCartProductsQuantity', () => {
     it('should return cart product quantities', () => {
-      const state = {
-        cart: {
+      const state = Map({
+        cart: ({
           addedIds: [ 1, 2, 3 ],
-          quantityById: {
+          quantityById: Map({
             1: 4,
             2: 2,
             3: 1
-          }
-        },
-        products: {
-          byId: {
-            1: {
+          })
+        }),
+        products: Map({
+          byId: Map({
+            1: Map({
               id: 1,
               price: 1.99
-            },
-            2: {
+            }),
+            2: Map({
               id: 1,
               price: 4.99
-            },
-            3: {
+            }),
+            3: Map({
               id: 1,
               price: 9.99
-            }
-          }
-        }
-      }
+            })
+          })
+        })
+      })
 
       expect(getCartProductsQuantity(state)).toEqual({
         1: 4,
